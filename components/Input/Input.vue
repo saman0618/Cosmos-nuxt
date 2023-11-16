e44fc972e13c7a762b29271b3b4412e6f4863459
Bakhtiyor <odilovskijarik@gmail.com>
2023-11-16 19:18:25 +0500

    Register

--- components/Input/Input.vue
+++ components/Input/Input.vue
@@ -1,6 +1,6 @@
 <template>
     <input :type="type"
-        class="input"
+        class="bg-white border focus:outline-none border-[#DBDBDB] text-[#7D7D88] text-sm rounded-lg block w-full py-3.5 px-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         :class="className"
         :placeholder="placeh">
 </template>
