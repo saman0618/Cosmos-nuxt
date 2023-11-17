4ea7774bf740a03b0198259941df7db5d3d071da
saman <samanmulla@icloud.com>
2023-11-18 01:50:14 +0500

    card finish

---
+++ components/Reaclamain/Reclamain.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+  name: "Reclamain",
+};
+
+</script>
+<template>
+
+<div class="px-8 pt-5">
+    <h1 class="text-[18px]">Siz o‘rganayapsiz</h1>
+    <div class="flex items-center rounded-xl bg-white  border-2  border-slate-300 p-4 mt-5">
+        <div class="">
+            <img src="@/assets/images/Ellipse 87.svg" alt="">
+        </div>
+        <div class="px-5">
+            <div class="py-2">
+                <h1 class="inline-block font-semibold text-md">Dasturlash</h1>
+                <span class="p-1 px-2 rounded-md bg-blue-100">1-modul</span>
+                <span class="p-1 px-2 rounded-md bg-blue-100">7-dars</span>
+            </div>
+            <span class="block bg-yellow-100 rounded-md p-2 rounded-md">Uyga vazifa topshirilmagan</span>
+        </div>
+        <div class="ml-auto">
+            <Button label="Davom Ettirish" />
+        </div>
+    </div>
+</div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,35 @@
 <template>
   <div class="flex h-[100vh]">
     <Sidebar />
-    <div class=" flex flex-col flex-1 ">
+    <div class="flex flex-col flex-1">
       <Header />
 
+      
       <div class="flex-3">
         <Nuxt />
-        <Kalendar />
 
       </div>
     </div>
   </div>
 </template>
-  
-  <style scoped>
-  .app-container {
-    display: flex;
-    /* flex-direction: column; */
-    height: 100vh;
-  }
-  
-  .content-container {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-  }
-  
-  .main-content {
-    flex: 3; /* Adjust the proportion as needed */
-    padding: 20px; /* Add padding as needed */
-  }
-  
-  /* Add any other styling for Header and Sidebar here */
-  </style>
-  
+
+<style scoped>
+.app-container {
+  display: flex;
+  /* flex-direction: column; */
+  height: 100vh;
+}
+
+.content-container {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.main-content {
+  flex: 3; /* Adjust the proportion as needed */
+  padding: 20px; /* Add padding as needed */
+}
+
+/* Add any other styling for Header and Sidebar here */
+</style>
